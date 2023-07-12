aefc8f70cdf383e9787f99b3154c25b01e142907
Maycol R <maycol.rodriguez.ma@gmail.com>
2023-07-11 19:33:42 -0500

    actualizacion del seminario

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slides = [
+    {
+      title: "Gatitos",
+      img: "https://www.zooplus.es/magazine/wp-content/uploads/2021/06/Lombrices-en-gatitos.jpg",
+      icon: "beer-outline",
+      description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las" 
+    },
+    {
+      title: "Perritos",
+      img: "assets/images/perritos.jpeg",
+      icon: "barbell-outline",
+      description: "Hola soy la explicacion del slide, Hola soy la explicacion del slide" 
+    },
+    {
+      title: "Hamsters",
+      img: "https://t1.ea.ltmcdn.com/es/posts/5/8/8/cuidados_y_alimentacion_del_hamster_7885_600.jpg",
+      icon: "beer-outline",
+      description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto" 
+    },
+    {
+      title: "Gatitos",
+      img: "https://www.zooplus.es/magazine/wp-content/uploads/2021/06/Lombrices-en-gatitos.jpg",
+      icon: "beer-outline",
+      description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las" 
+    }
+  ]
+
+  constructor(private router: Router, private storage: Storage) { }
+
+  ngOnInit() {
+  }
+
+  close() {
+    console.log("estoy tratando de cerrar");
+    this.storage.set("introShow", true);
+    this.router.navigateByUrl("/home");
+  }
+
+}
